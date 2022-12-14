9cceb27a1af276f8ab90650b2c793790d2c542c0
Tony Qu <tonyqus@gmail.com>
2022-12-14 20:31:50 +0800

    enable localizaion of faq page

--- web/src/components/Faq/FaqPage.tsx
+++ web/src/components/Faq/FaqPage.tsx
@@ -4,10 +4,12 @@ import { Col, Container, Row } from 'reactstrap'
 import { CenteredEditable } from 'src/components/Common/Editable'
 import { PageHeading } from 'src/components/Common/PageHeading'
 import { Layout } from 'src/components/Layout/Layout'
-
+import { useRouter } from "next/router";
 import Faq from '../../../../content/Faq.md'
+import Faq_zh from '../../../../content/Faq_zh-CN.md'
 
 export function FaqPage() {
+  const { locale} = useRouter();
   return (
     <Layout>
       <Container>
@@ -20,7 +22,9 @@ export function FaqPage() {
         <Row noGutters>
           <Col>
             <CenteredEditable githubUrl="blob/master/content/Faq.md">
-              <Faq />
+            {
+                locale== "zh-CN"? <Faq_zh />:<Faq />
+            }
             </CenteredEditable>
           </Col>
         </Row>
